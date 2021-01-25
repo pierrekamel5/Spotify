e78b7d10b40c4c172ac9b055a74cb4e0679a104e
Pierre <Pierre>
2021-01-25 11:47:50 +0200

    Spotify Demo

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Shopifydemo'),
+      dir: require('path').join(__dirname, './coverage/Spotifydemo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
